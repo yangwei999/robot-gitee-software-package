230642af61a9cc53eb244e0704569a74b99f6089
yangwei999 <348134071@qq.com>
2023-11-22 15:21:56 +0800

    Fix no sig (#57)
    * fix no sig
    
    * add cpio

--- softwarepkg/infrastructure/repositoryimpl/software_pkg_pr_do.go
+++ softwarepkg/infrastructure/repositoryimpl/software_pkg_pr_do.go
@@ -11,9 +11,12 @@ import (
 
 type SoftwarePkgPRDO struct {
 	// must set "uuid" as the name of column
-	PkgId   uuid.UUID `gorm:"column:uuid;type:uuid"`
-	Link    string    `gorm:"column:link"`
-	PkgName string    `gorm:"column:pkg_name"`
+	PkgId    uuid.UUID `gorm:"column:uuid;type:uuid"`
+	Link     string    `gorm:"column:link"`
+	PkgName  string    `gorm:"column:pkg_name"`
+	PkgDesc  string    `gorm:"column:pkg_desc"`
+	Upstream string    `gorm:"column:upstream"`
+	Sig      string    `gorm:"column:sig"`
 	//Num is the pr num of community repo
 	Num           int    `gorm:"column:num"`
 	CIPRNum       int    `gorm:"column:ci_pr_num"`
@@ -39,6 +42,9 @@ func (s softwarePkgPR) toSoftwarePkgPRDO(p *domain.SoftwarePkg, id uuid.UUID, do
 		Status:        p.Status,
 		Link:          p.PullRequest.Link,
 		PkgName:       p.Name,
+		PkgDesc:       p.Application.PackageDesc,
+		Upstream:      p.Application.Upstream,
+		Sig:           p.Application.ImportingPkgSig,
 		ImporterName:  p.Importer.Name,
 		ImporterEmail: email,
 		SpecURL:       p.Application.SourceCode.SpecURL,
@@ -64,6 +70,9 @@ func (do *SoftwarePkgPRDO) toDomainPullRequest() (pkg domain.SoftwarePkg, err er
 	pkg.Importer.Name = do.ImporterName
 	pkg.Application.SourceCode.SpecURL = do.SpecURL
 	pkg.Application.SourceCode.SrcRPMURL = do.SrcRPMURL
+	pkg.Application.PackageDesc = do.PkgDesc
+	pkg.Application.Upstream = do.Upstream
+	pkg.Application.ImportingPkgSig = do.Sig
 
 	return
 }
